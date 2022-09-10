c2641c9dfea7cad1ccb7980a81858be4650da115
Peter Cox <pcox@contentformula.com>
2022-09-10 21:03:46 +0100

    Working redux with works API

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -2,9 +2,7 @@ import React, { useEffect } from "react";
 import axios from "axios";
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchRecipes, recipesSelector } from '../slices/recipes'
-
-
-
+import { fetchWorks, worksSelector } from '../slices/works';
 import './HelloWorld.scss'
 import { Author } from "../models/Author";
 import { ExpandLevel } from "../models/Misc";
@@ -14,32 +12,50 @@ import { debounce } from "lodash";
 const HelloWorld = () =>
 {
 
-  const dispatch: any = useDispatch()
-  const { recipes, loading, hasErrors } = useSelector(recipesSelector);
+  const dispatch: any = useDispatch();
+
+  const { recipes, loadingRecipes, recipesHasErrors } = useSelector(recipesSelector);
+  const { works, loadingWorks, worksHasErrors } = useSelector(worksSelector);
+
 
   useEffect(() =>
   {
+    dispatch(fetchWorks())
     dispatch(fetchRecipes())
   }, [dispatch])
 
   const renderRecipes = () =>
   {
-    if (loading) return <p>Loading recipes...</p>
-    if (hasErrors) return <p>Cannot display recipes...</p>
+    if (loadingRecipes) return <p>Loading recipes..</p>
+    if (recipesHasErrors) return <p>Cannot display recipes...</p>
 
     return recipes.map((recipe: any) =>
       <div key={recipe.idMeal} className='tile'>
         <h2>{recipe.strMeal}</h2>
-        <img src={recipe.strMealThumb} alt='' />
       </div>
     )
   }
 
+  const renderWorks = () =>
+  {
+    if (loadingWorks) return <p>Loading works...</p>
+    if (worksHasErrors) return <p>Cannot display works...</p>
+
+    return works.map((work: Work) => {
+      return (
+        <p>
+          {work.titleAuth}
+        </p>
+        )
+      });
+  }
+
+
   return (
     <section>
-      <h1>Cum</h1>
+      <h1>Hello</h1>
       <div className='content'>
-        {renderRecipes()}
+        {renderWorks()}      
       </div>
     </section>
   )
--- src/slices/works.ts
+++ src/slices/works.ts
@@ -1,4 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
+import axios from 'axios'
+import { ExpandLevel } from '../models/Misc'
 
 export const initialState = {
   loading: false,
@@ -33,24 +35,51 @@ const worksSlice = createSlice({
 export const { getWorks, getWorksSuccess, getWorksFailure } = worksSlice.actions
 
 // A selector
-export const worksSelector = (state: any) => state.recipes
+export const worksSelector = (state: any) => state.works
 
 // The reducer
 export default worksSlice.reducer
 
 // Asynchronous thunk action
-export function fetchRecipes()
+export function fetchWorks()
 {
   return async (dispatch: any) =>
   {
     dispatch(getWorks())
 
+
     try
     {
-      const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
-      const data = await response.json()
+      //Works
+      let works: any = [];
 
-      dispatch(getWorksSuccess(data.meals))
+      const getWorks = async (start: number, end: number, expandLevel: ExpandLevel, search: string): Promise<void> => {
+        var config = {
+          method: 'get',
+          url: `https://reststop.randomhouse.com/resources/works?
+            start=${start}&
+            max=${end}&
+            expandLevel=${expandLevel}&
+            search=${search}`.replace(/\s/g, ""),
+          headers: { 
+            'Accept': 'application/json'
+          }
+        };
+        
+        axios(config)
+        .then((response) => {
+          console.log(response)
+          works = response.data.work;
+          dispatch(getWorksSuccess(works))
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      }
+      await getWorks(0, 10, ExpandLevel.LinksAndDetails, "Harry");
+      // console.log("WORKS");
+
+
     } catch (error)
     {
       dispatch(getWorksFailure())
